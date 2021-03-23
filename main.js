16546ceb1cea2fb6cb7bef01ab281c35585fec42
lzg0321 <lzg2903@gmail.com>
2021-03-23 23:44:40 +0800

    test

--- main.js
+++ main.js
@@ -2,5 +2,9 @@
 import LedgerBridge from './LedgerBridge'
 (async () => {
   const bridge = new LedgerBridge()
+  window.bridge = bridge;
+  window.onclick = function () {
+    window.bridge.unlock();
+  }
 })()
-console.log('Mina < = > Ledger Bridge initialized!')
+console.log('Mina < = > Ledger Bridge initialized!!')
